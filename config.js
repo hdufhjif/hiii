e7857fe21fecce88a24cb42af3b318b7a618c389
hdufhjif <121323029+hdufhjif@users.noreply.github.com>
2023-01-04 18:55:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.Prefix = `HaaaMa / Prefix`;
-exports.Token = `HaaaMa / Token`;
+exports.Prefix = `2`;
+exports.Token = `MTA2MDIyNDMzMzkxOTgxNzc2MA.GuE3cH.pL0rg3UcOng65RwuxKZ-e76dSLH1RhSpnCDV3A`;
 //// token dabe Bashi haaama.js dabne
